a6bf3d33dfdbf3fc2abe2d67c70ebcf7a8dc4cb3
Mark McFate <mcfatem@grinnell.edu>
2016-03-29 14:24:45 -0500

    Re-introduce the ICU and IDU modules.

---
+++ scripts/custom/idu.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# idu.sh
+#
+# When placed in the ../scripts/custom directory this script will is automatically invoked by
+# ../scripts/custom to load and configure Digital Grinnell's Islandora Drush Utilities (idu)
+# module from https://github.com/DigitalGrinnell/idu.
+#
+# Note that idu is dependent upon the icu (Islandora Common Utilities) module.
+
+# Changes:
+# 29-Mar-2016 - Initial merge.
+#
+
+echo "Installing the Digital Grinnell IDU module per idu.sh."
+
+SHARED_DIR=$1
+
+if [ -f "$SHARED_DIR/configs/variables" ]; then
+  # shellcheck source=/dev/null
+  . "$SHARED_DIR"/configs/variables
+fi
+
+# Clone custom modules from GitHub
+echo "Cloning DG's idu Module."
+cd "$DRUPAL_HOME"/sites/all/modules || exit
+git clone https://github.com/DigitalGrinnell/idu
+cd idu
+git config core.filemode false
+
+# Enable the custom modules
+drush -y -u 1 en idu
+
+cd "$DRUPAL_HOME"/sites/all/modules || exit
+
+# Permissions and ownership
+sudo chown -hR vagrant:www-data "$DRUPAL_HOME"/sites/all/modules
+sudo chmod -R 755 "$DRUPAL_HOME"/sites/all/modules
+
