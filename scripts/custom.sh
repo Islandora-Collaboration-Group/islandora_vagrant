7e202a8d139ad29b96236385af0031463903435b
Benjamin Rosner <brosner@barnard.edu>
2016-04-14 16:51:15 -0400

    Travis Fix 5

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -23,17 +23,16 @@ fi
 
 # Now, check for a ../config/custom_variables and pull definitions from it if found.
 if [ -f "$SHARED_DIR/configs/custom_variables" ]; then
-  echo "Invoking ../configs/custom_variables."
   . "$SHARED_DIR"/configs/custom_variables
+  echo "Invoking ../configs/custom_variables."
 fi
 
 # Run all the scripts (*.sh) found in ../scripts/custom/.
 for SCRIPT in ${SHARED_DIR}/scripts/custom/*.sh
 do
-  if [ -f $SCRIPT ]
-  then
-    echo "Custom.sh is invoking script '$SCRIPT'..."
-	source $SCRIPT
+  if [ -f "$SCRIPT" ]; then
+	source "$SCRIPT"
+	echo "Custom.sh is invoking script '$SCRIPT'..."
   fi
 done
 
